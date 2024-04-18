2721f9790a6779bb65bbeb3272606160ef833dc5
Steve Wright <steve@mac-pro.local>
2024-04-18 18:56:26 +0100

    added styles to builder

--- htdocs/wifi-portal/inc/_builder.php
+++ htdocs/wifi-portal/inc/_builder.php
@@ -12,3 +12,16 @@ $_SESSION['profile'] = array('venue' => array('serivce_id' => 0, 'location_id'	=
 $json = json_encode($_SESSION['profile'], JSON_PRETTY_PRINT);
 
 echo '<pre>'.$json.'</pre>';	
+
+?>
+<style>
+body { color: <?php echo $ui['css_canvas_txt_hex'];?>; background: <?php echo $ui['css_page_bg_hex'];?>; } 
+.link { color: <?php echo $ui['css_canvas_link_hex'];?>; }
+.link:hover { color: <?php echo $ui['css_canvas_link_hover_hex'];?>; }
+.launch-screen { color: <?php echo $ui['css_canvas_txt_hex'];?>; background: url('<?php echo $ui['css_page_bg_img'];?>') center center;}
+.master-button { border-color: <?php echo $ui['css_canvas_btn_hex'];?>; background: <?php echo $ui['css_canvas_btn_hex'];?>; }
+.master-button:hover { border-color: <?php echo $ui['css_canvas_btn_hover_hex'];?>; background: <?php echo $ui['css_canvas_btn_hover_hex'];?>; }
+h2 { color: <?php echo $ui['css_canvas_title_hex'];?>; } 
+.offcanvas, .offcanvas.sm { border: 3px solid <?php echo $ui['css_canvas_brdr_hex'];?>; background: <?php echo $ui['css_canvas_bg_hex'];?>; }
+p { color: <?php echo $ui['css_canvas_intro_hex'];?>;}
+</style>
