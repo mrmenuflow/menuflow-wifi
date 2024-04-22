a6e55d2b5dbe5500817c7e11de0707c819fb1129
Steve Wright <steve@mac-pro.local>
2024-04-22 17:25:49 +0100

    added redirect

--- htdocs/wifi-portal/inc/_global.js
+++ htdocs/wifi-portal/inc/_global.js
@@ -106,7 +106,7 @@ $.ajax({
 					type: 'POST',
 					data: { action: 'auth_guest' },
 					success: function (rsp) { 
-						alert(rsp)
+						window.location.replace("http://google.com");
 					}
 				});
 			}
